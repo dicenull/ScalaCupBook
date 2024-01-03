15a9794577d146ec598512344942b85a076375b5
dicenull <dicenull.dev@gmail.com>
2024-01-03 21:36:05 +0900

    add above/beside

--- codes/Layout.scala
+++ codes/Layout.scala
@@ -2,6 +2,18 @@ abstract class Element {
   def contents: Array[String]
   def height: Int = contents.length
   def width: Int = if (height == 0) 0 else contents(0).length
+
+  def above(that: Element): Element = {
+    new ArrayElement(this.contents ++ that.contents)
+  }
+
+  def beside(that: Element): Element = {
+    val contents = new Array[String](this.contents.length)
+    for (i <- 0 until this.contents.length)
+      contents(i) = this.contents(i) + that.contents(i)
+
+    new ArrayElement(contents)
+  }
 }
 
 class ArrayElement(val contents: Array[String]) extends Element
