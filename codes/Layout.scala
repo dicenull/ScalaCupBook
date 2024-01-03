552bfa6b7f992fb769b288eaa9aa0cf91135478a
dicenull <dicenull.dev@gmail.com>
2024-01-03 21:51:18 +0900

    refactor beside

--- codes/Layout.scala
+++ codes/Layout.scala
@@ -8,11 +8,11 @@ abstract class Element {
   }
 
   def beside(that: Element): Element = {
-    val contents = new Array[String](this.contents.length)
-    for (i <- 0 until this.contents.length)
-      contents(i) = this.contents(i) + that.contents(i)
-
-    new ArrayElement(contents)
+    new ArrayElement(
+      for {
+        (line1, line2) <- this.contents zip that.contents
+      } yield line1 + line2
+    )
   }
 }
 
