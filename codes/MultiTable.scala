dc01a02725550873f5fba2263ba1bf4c05bed450
dicenull <dicenull.dev@gmail.com>
2024-01-03 14:00:00 +0900

    row sequence

--- codes/MultiTable.scala
+++ codes/MultiTable.scala
@@ -1,13 +1,13 @@
+def makeRowSeq(row: Int) =
+  for (col <- 1 to 10) yield {
+    val prod = (row * col).toString
+    val padding = " " * (4 - prod.length)
+    padding + prod
+  }
+
 def printMultiTable(): Unit = {
-
   for (i <- 1 to 10) {
-    for (j <- 1 to 10) {
-
-      val prod = (i * j).toString
-
-      val padding = " " * (4 - prod.length)
-      print(padding + prod)
-    }
+    print(makeRowSeq(i).mkString)
 
     println()
   }
