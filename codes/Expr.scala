02563a6dc830a9dcc48832e07c19343cbb84b7d2
dicenull <dicenull.dev@gmail.com>
2024-01-06 10:52:40 +0900

    add expr

---
+++ codes/Expr.scala
@@ -0,0 +1,21 @@
+sealed abstract class Expr
+case class Var(name: String) extends Expr
+case class Number(num: Double) extends Expr
+case class UnOp(operator: String, arg: Expr) extends Expr
+case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
+
+def simplifyTop(expr: Expr): Expr = expr match {
+  case UnOp("-", UnOp("-", e))  => e
+  case BinOp("+", e, Number(0)) => e
+  case BinOp("*", e, Number(1)) => e
+  case _                        => expr
+}
+
+def simplifyAll(expr: Expr): Expr = expr match {
+  case UnOp("-", UnOp("-", e))  => simplifyAll(e)
+  case BinOp("+", e, Number(0)) => simplifyAll(e)
+  case BinOp("*", e, Number(1)) => simplifyAll(e)
+  case UnOp(op, e)              => UnOp(op, simplifyAll(e))
+  case BinOp(op, l, r)          => BinOp(op, simplifyAll(l), simplifyAll(r))
+  case _                        => expr
+}
